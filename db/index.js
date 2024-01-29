281cd5f5c581a516474124930e7b7e74a195f44d
Mathieu Saubin <37439688+Doomfang3@users.noreply.github.com>
2024-01-29 15:52:07 +0100

    Fix DB connection catch

--- db/index.js
+++ db/index.js
@@ -15,7 +15,7 @@ const withDB = async serverListener => {
       serverListener()
     }
   } catch (error) {
-    console.error('Error connecting to mongo: ', err)
+    console.error('Error connecting to mongo: ', error)
   }
 }
 
